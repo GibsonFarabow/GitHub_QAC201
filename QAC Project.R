59df87ce609b2c5e9bcf6836161efb91b9a6e4b3
Gibson Farabow <gibsonfarabow@gmail.com>
2019-03-21 02:52:08 -0400

    Update QAC Project.R

--- QAC Project.R
+++ QAC Project.R
@@ -2,14 +2,14 @@
 # QAC 201
 
 library(ggplot2)
-library(descr)
+library(descr) 
 library(fifer)
 
-setwd("/Users/gibsonfarabow/Desktop/GitHub_QAC201") 
+setwd("/Users/gibsonfarabow/Desktop/GitHub_QAC201")
 
 #load("/Users/gibsonfarabow/Desktop/GitHub_QAC201/QAC201/OutlookOnLifeData/OOL_PDSwoLabels.RData")
 #OOL_Labels <- OOL_PDSwoLabels
-load("/Users/gibsonfarabow/Desktop/GitHub_QAC201/QAC201/OutlookOnLifeData/OOL_PDS.RData")
+load("/Users/gibsonfarabow/Desktop/GitHub_QAC201/QAC201/OOL_Data_Info/OOL_PDS.RData")
 Data <- OOL_PDS
 
 # Subsetting to create a smaller dataset which contains all variables of interest
@@ -19,13 +19,13 @@ myData <- Data[c("ppagect4","ppeducat","ppethm","ppgender","ppincimp","ppmsacat"
                  "w1_a1", "w1_a10", "w1_a11", "w1_a12a", "w1_b1", "w1_b2", "w1_b4", "w1_c1",
                  "w1_c2", "w1_e1", "w1_f3", "w1_g2", "w1_g3a", "w1_i1", "w1_i2", "w1_j3a_a",
                  "w1_j3a_b","w1_j3a_c", "w1_k1_a", "w1_k1_b", "w1_k1_c", "w1_k1_d", "w1_l2_1",
-                 "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2", 
+                 "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2",
                  "w1_p4", "w1_p5", "w1_p13a", "w1_p14", "w1_p15", "w1_p20",
-                 
+
                  "w1_d11", "w1_d12", "w1_d13", "w1_d14", "w1_d15", "w1_d16", "w1_d17", "w1_d18",
                  "w1_d19", "w1_d20",
                  "w2_qb1c", "w2_qb1d", "w2_qb2a", "w2_qf10d", "w2_qi3", "w2_qi4_c", "w2_qk3",
-                 "w2_qm12", "w2_qn3", "w2_qn4")] 
+                 "w2_qm12", "w2_qn3", "w2_qn4")]
 
 # copy for managing NAs
 myData2 <- Data[c("ppagect4","ppeducat","ppethm","ppgender","ppincimp","ppmsacat","pphhsize","ppreg4",
@@ -33,20 +33,20 @@ myData2 <- Data[c("ppagect4","ppeducat","ppethm","ppgender","ppincimp","ppmsacat
                             "w1_a1", "w1_a10", "w1_a11", "w1_a12a", "w1_b1", "w1_b2", "w1_b4", "w1_c1",
                             "w1_c2", "w1_e1", "w1_f3", "w1_g2", "w1_g3a", "w1_i1", "w1_i2", "w1_j3a_a",
                             "w1_j3a_b","w1_j3a_c", "w1_k1_a", "w1_k1_b", "w1_k1_c", "w1_k1_d", "w1_l2_1",
-                            "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2", 
+                            "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2",
                             "w1_p4", "w1_p5", "w1_p13a", "w1_p14", "w1_p15", "w1_p20",
-                            
+
                             "w1_d11", "w1_d12", "w1_d13", "w1_d14", "w1_d15", "w1_d16", "w1_d17", "w1_d18",
                             "w1_d19", "w1_d20",
                             "w2_qb1c", "w2_qb1d", "w2_qb2a", "w2_qf10d", "w2_qi3", "w2_qi4_c", "w2_qk3",
-                            "w2_qm12", "w2_qn3", "w2_qn4")] 
+                            "w2_qm12", "w2_qn3", "w2_qn4")]
 
 Wave2 <- Data[c("w2_qb1c", "w2_qb1d", "w2_qb2a", "w2_qf10d", "w2_qi3", "w2_qi4_c", "w2_qk3",
               "w2_qm12", "w2_qn3", "w2_qn4")]
 Wave1 <- Data[c("w1_a1", "w1_a10", "w1_a11", "w1_a12a", "w1_b1", "w1_b2", "w1_b4", "w1_c1",
                 "w1_c2", "w1_e1", "w1_f3", "w1_g2", "w1_g3a", "w1_i1", "w1_i2", "w1_j3a_a",
                 "w1_j3a_b","w1_j3a_c", "w1_k1_a", "w1_k1_b", "w1_k1_c", "w1_k1_d", "w1_l2_1",
-                "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2", 
+                "w1_l2_2", "w1_l2_3", "w1_m1", "w1_m3","w1_m7", "w1_o1","w1_p2",
                 "w1_p4", "w1_p5", "w1_p13a", "w1_p14", "w1_p15", "w1_p20",
                 "w1_d11", "w1_d12", "w1_d13", "w1_d14", "w1_d15", "w1_d16", "w1_d17", "w1_d18",
                 "w1_d19", "w1_d20")]
@@ -117,9 +117,9 @@ myData$religion.a[myData$w1_m1 == 1] <- "Baptist (any)"
 myData$religion.a[myData$w1_m1 == 2] <- "Protestant"
 myData$religion.a[myData$w1_m1 == 3] <- "Catholic"
 myData$religion.a[myData$w1_m1 == 13] <- "None"
-myData$religion.a[myData$w1_m1 == 4 | 
+myData$religion.a[myData$w1_m1 == 4 |
   myData$w1_m1 == 9 | myData$w1_m1 ==10 | myData$w1_m1 == 11] <- "Other Christian"
-myData$religion.a[myData$w1_m1 !=1 & myData$w1_m1 !=2 & myData$w1_m1 !=3 & myData$w1_m1 !=4 & 
+myData$religion.a[myData$w1_m1 !=1 & myData$w1_m1 !=2 & myData$w1_m1 !=3 & myData$w1_m1 !=4 &
   myData$w1_m1 !=9 & myData$w1_m1 !=10 & myData$w1_m1 !=11 & myData$w1_m1 !=13 ] <- "Other Religion"
 
 myData$religion.a <- as.factor(myData$religion.a)
@@ -144,7 +144,7 @@ barplot(table(myData$sub_Rel), main="Politics and Church", ylab="Frequency",
 barplot(table(myData$religion.a), main="Religion", ylab="Frequency")
 
 ggplot(myData) +
-  geom_bar(aes(x=pol_party)) + 
+  geom_bar(aes(x=pol_party)) +
   geom_label(stat="count", aes(x=pol_party, label=..count..))
 
 
@@ -158,14 +158,14 @@ ggplot(data=myData) + geom_bar(aes(x=sub_Rel, y=Cong_Rep_Score), stat="summary",
 
 ggplot(data=subset(myData, !is.na(myData$Polit_Int))) + stat_summary(aes(x=Polit_Int, y=sub_Rel), fun.y=mean, geom="bar")
 # Shows clear positive correlation between political interest and belief that
-# churches should be involved in politics  
+# churches should be involved in politics
 
 ggplot(data=subset(myData, !is.na(myData$Polit_Int)),
        aes(x=Polit_Int, y=sub_Rel, fill = religion.a)) +
   geom_bar(stat = "summary", fun.y = "mean") #+ geom_text(label=sub_Rel)
-# use position "dodge" to make bars beside each other  
+# use position "dodge" to make bars beside each other
 
-# same data different graph below                                   
+# same data different graph below
 ggplot(data=subset(myData, !is.na(myData$Polit_Int))) +
   geom_boxplot(aes(x=Polit_Int, y=sub_Rel)) +
   facet_grid(. ~religion.a)
@@ -181,35 +181,35 @@ TukeyHSD(Cong_Pol.aov)
 # Conducting an ANOVA test indicates that there is a significant association
 # between political interest and feelings. Running a post-hoc test indicates
 # there is only a significant correlation between political interest and feelings toward Congress
-# when 1: comparing no political interest ("None") and "Moderate," 
-# 2: comparing "Slight" to "None," and 
+# when 1: comparing no political interest ("None") and "Moderate,"
+# 2: comparing "Slight" to "None," and
 # 3: comparing "Very" to "None."
 # These results show that there is only a significant association between Political Interest
 # and faith in Congress when measuring political interest in comparison to no political interest,
 # however even here the relationship is not linear. (I will conduct a regular correlation later, because
-# I think it makes more sense to make political interest a numerical value instead of a factor.)    
+# I think it makes more sense to make political interest a numerical value instead of a factor.)
 
 #Visualize the relationship for correlation
 ggplot(myData, aes(x=Polit_Int_Num, y=Cong_Rep_Score)) + geom_jitter() + stat_smooth(method="lm")
-cor.test(myData$Polit_Int_Num, myData$Cong_Rep_Score) 
+cor.test(myData$Polit_Int_Num, myData$Cong_Rep_Score)
 # Shows there is not a significant correlation between Political Interest and Congressional score
 # r = -.086, p < .001
 
 
 cor.test(myData$Polit_Int_Num, myData$sub_Rel)
 # Shows there is a weak positive correlation between political interest and
-# belief in the seperation of church and politics; r = .124, p < .001 
+# belief in the seperation of church and politics; r = .124, p < .001
 Pol_Sub.aov <- aov(myData$sub_Rel ~ myData$Polit_Int, data=myData)
 summary(Pol_Sub.aov)
 TukeyHSD(Pol_Sub.aov)
 # Again mostly shows a significant relationship between people with 'None'
-# political interest vs others. Also shows significant relationship between people 
-# 'Very' interested in politics vs 'Extremely.' 
+# political interest vs others. Also shows significant relationship between people
+# 'Very' interested in politics vs 'Extremely.'
 
 ##############################################################################################
 ggplot(myData, aes(x=sub_Rel, y=Cong_Rep_Score)) + geom_jitter() + stat_smooth(method="lm")
 cor.test(myData$sub_Rel, myData$Cong_Rep_Score)
-# weak correlation (r = .111), which shows a statistically significant positive relation with the belief 
+# weak correlation (r = .111), which shows a statistically significant positive relation with the belief
 # that church and polilitcs can go together and Congress score
 
 Party_Cong.aov <- aov(myData$Cong_Rep_Score ~ myData$pol_party)
@@ -217,7 +217,7 @@ summary(Party_Cong.aov)
 TukeyHSD(Party_Cong.aov)
 # There is only a significant difference between Democrats and Indendents for Congress Score
 
-Party_sub_Rel.aov <- aov(myData$sub_Rel ~ myData$pol_party)
+Party_subRel.aov <- aov(myData$sub_Rel ~ myData$pol_party)
 summary(Party_subRel.aov)
 TukeyHSD(Party_subRel.aov)
 # Significant difference between everyone but Republican vs Independent; p < .005
@@ -238,6 +238,3 @@ ggplot(party_graph, aes(x=Polit_Int, y=Cong_Rep_Score, fill=Pol_Party)) +
   stat_summary(position="dodge", geom="bar", fun.y="mean")
 ggplot(party_graph) + stat_summary(aes(x=Pol_Party, y=Polit_Int), geom="bar")
 # probably no significant different in interest except maybe for Independents
-
-
-
